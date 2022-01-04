1eb80cec78de4a937ba228eb7aa3e54073fb5d7c
Joseph Ortiz <JoeProgrammer88@users.noreply.github.com>
2022-01-04 10:05:57 -0800

    Added greeting

--- BranchingPractice/Form1.Designer.cs
+++ BranchingPractice/Form1.Designer.cs
@@ -28,19 +28,44 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.btnDisplayGreeting = new System.Windows.Forms.Button();
+            this.txtName = new System.Windows.Forms.TextBox();
             this.SuspendLayout();
+            // 
+            // btnDisplayGreeting
+            // 
+            this.btnDisplayGreeting.Location = new System.Drawing.Point(65, 126);
+            this.btnDisplayGreeting.Name = "btnDisplayGreeting";
+            this.btnDisplayGreeting.Size = new System.Drawing.Size(100, 62);
+            this.btnDisplayGreeting.TabIndex = 0;
+            this.btnDisplayGreeting.Text = "Display Greeting";
+            this.btnDisplayGreeting.UseVisualStyleBackColor = true;
+            this.btnDisplayGreeting.Click += new System.EventHandler(this.btnDisplayGreeting_Click);
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(65, 81);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(100, 23);
+            this.txtName.TabIndex = 1;
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.btnDisplayGreeting);
             this.Name = "Form1";
             this.Text = "Branching Practice";
             this.ResumeLayout(false);
+            this.PerformLayout();
 
         }
 
         #endregion
+
+        private Button btnDisplayGreeting;
+        private TextBox txtName;
     }
 }
--- BranchingPractice/Form1.cs
+++ BranchingPractice/Form1.cs
@@ -16,5 +16,10 @@ namespace BranchingPractice
         {
             InitializeComponent();
         }
+
+        private void btnDisplayGreeting_Click(object sender, EventArgs e)
+        {
+            MessageBox.Show($"Hello {txtName.Text}");
+        }
     }
 }
